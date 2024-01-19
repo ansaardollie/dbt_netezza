ede46856e0cf44309bbd75eeb85b3271830a7586
Joe Mirizio <mirizioj@chop.edu>
2024-01-19 09:37:20 -0500

    Fix split_part macro to support new test case (#88)

--- dbt/include/netezza/macros/utils/split_part.sql
+++ dbt/include/netezza/macros/utils/split_part.sql
@@ -1,3 +1,7 @@
 {% macro netezza__split_part(string_text, delimiter_text, part_number) %}
-    get_value_varchar(array_split({{string_text}}, {{delimiter_text}}), {{part_number}})
+    {% if part_number >= 0 %}
+        get_value_varchar(array_split({{string_text}}, {{delimiter_text}}), {{part_number}})
+    {% else %}
+        get_value_varchar(array_split({{string_text}}, {{delimiter_text}}), array_count(array_split({{string_text}}, {{delimiter_text}})) + 1 {{part_number}})
+    {% endif %}
 {% endmacro %}
