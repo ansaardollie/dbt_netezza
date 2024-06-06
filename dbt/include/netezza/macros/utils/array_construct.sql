226f25e2278a2f7092b2a1942864c2ff887fb661
Joe Mirizio <mirizioj@chop.edu>
2024-06-06 19:24:06 +0000

    Fix array utils macros

--- dbt/include/netezza/macros/utils/array_construct.sql
+++ dbt/include/netezza/macros/utils/array_construct.sql
@@ -1,25 +1,35 @@
 {% macro netezza__array_instantiate(data_type) -%}
-    {% set type_int_mapping = {
-    "INT1": 1,
-    "INT2": 2,
-    "INT": 3,
-    "BIGINT": 4,
-    "DATE": 5,
-    "TIME": 6,
-    "TIMESTAMP": 7,
-    "VARCHAR": 8,
-    "NVARCHAR": 9,
-    "FLOAT": 10,
-    "DOUBLE": 11,
-    "TIMETZ": 15
-    } %}
-    array({{ type_int_mapping[data_type] }})
+    {%- set array_type_mapping = {
+        "int1": 1,
+        "byteint": 1,
+        "int2": 2,
+        "smallint": 2,
+        "int4": 3,
+        "int": 3,
+        "integer": 3,
+        "int8": 4,
+        "bigint": 4,
+        "date": 5,
+        "time": 6,
+        "timestamp": 7,
+        "varchar": 8,
+        "nvarchar": 9,
+        "float": 10,
+        "double": 11,
+        "timetz": 15
+    } -%}
+    {%- set type_code = array_type_mapping[data_type|lower] -%}
+    {%- if not type_code -%}
+        {{ exceptions.raise_compiler_error("Array data_type ('" ~ data_type ~ "') must be a valid type (https://www.ibm.com/docs/en/netezza?topic=functions-array).") }}
+    {%- else -%}
+        array({{ type_code }})
+    {%- endif -%}
 {%- endmacro %}
 
 {% macro netezza__array_construct(inputs, data_type) -%}
-    {% if inputs|length == 0 %}
+    {% if inputs|length == 0 -%}
         {{netezza__array_instantiate(data_type)}}
-    {% else %}
-        {{array_append(array_construct(inputs[:-1]), inputs[-1])}}
-    {% endif %}
+    {%- else -%}
+        {{array_append(array_construct(inputs[:-1], data_type), inputs[-1])}}
+    {%- endif %}
 {%- endmacro %}
